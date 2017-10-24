29d34c6202e75ad4f0840e361b5b4035078d907d
Michael Rans <rans@email.com>
2017-10-24 11:45:20 +0200

    use with

--- run.py
+++ run.py
@@ -22,25 +22,25 @@ def main():
     """Generate dataset and create it in HDX"""
 
     base_url = Configuration.read()['base_url']
-    downloader = Download(extra_params_yaml=join(expanduser("~"), 'idmc.yml'))
-    endpoints = Configuration.read()['endpoints']
-    tags = Configuration.read()['tags']
-    datasets, showcase = generate_indicator_datasets_and_showcase(base_url, downloader, endpoints, tags)
-    showcase.create_in_hdx()
-    for dataset in datasets.values():
-        dataset.update_from_yaml()
-        dataset.create_in_hdx()
-        showcase.add_dataset(dataset)
-
-    countriesdata = get_countriesdata(base_url, downloader)
-    logger.info('Number of datasets to upload: %d' % len(countriesdata))
-    for countrydata in countriesdata:
-        dataset, showcase = generate_country_dataset_and_showcase(base_url, downloader, datasets, countrydata, endpoints, tags)
-        if dataset:
+    with Download(extra_params_yaml=join(expanduser("~"), 'idmc.yml')) as downloader:
+        endpoints = Configuration.read()['endpoints']
+        tags = Configuration.read()['tags']
+        datasets, showcase = generate_indicator_datasets_and_showcase(base_url, downloader, endpoints, tags)
+        showcase.create_in_hdx()
+        for dataset in datasets.values():
             dataset.update_from_yaml()
             dataset.create_in_hdx()
-            showcase.create_in_hdx()
             showcase.add_dataset(dataset)
+
+        countriesdata = get_countriesdata(base_url, downloader)
+        logger.info('Number of datasets to upload: %d' % len(countriesdata))
+        for countrydata in countriesdata:
+            dataset, showcase = generate_country_dataset_and_showcase(base_url, downloader, datasets, countrydata, endpoints, tags)
+            if dataset:
+                dataset.update_from_yaml()
+                dataset.create_in_hdx()
+                showcase.create_in_hdx()
+                showcase.add_dataset(dataset)
 
 if __name__ == '__main__':
     facade(main, hdx_site='test', project_config_yaml=join('config', 'project_configuration.yml'))
